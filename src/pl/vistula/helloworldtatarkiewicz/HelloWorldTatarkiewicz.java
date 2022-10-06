c747b355f04117134d2967586c080d5d3ebbd995
Piotr <tatarkiewicz15@gmail.com>
2022-10-06 16:15:40 +0200

    commit 1

--- src/pl/vistula/helloworldtatarkiewicz/HelloWorldTatarkiewicz.java
+++ src/pl/vistula/helloworldtatarkiewicz/HelloWorldTatarkiewicz.java
@@ -3,6 +3,10 @@ package pl.vistula.helloworldtatarkiewicz;
 public class HelloWorldTatarkiewicz {
     public static void main(String[] args) {
         System.out.println("HelloVistulaTatarkiewcz");
-       
+        System.out.println("CosCos");
+        System.out.println("12345");
+        System.out.println("Java1");
+        System.out.println("Java2");
+        System.out.println("Java3");
     }
 }
