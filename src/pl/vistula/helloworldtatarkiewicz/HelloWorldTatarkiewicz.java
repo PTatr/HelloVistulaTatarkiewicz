901b55ac2a16a2be324bd3eb639e16ed60c358d5
Piotr <tatarkiewicz15@gmail.com>
2022-10-06 16:13:28 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldtatarkiewicz/HelloWorldTatarkiewicz.java
@@ -0,0 +1,8 @@
+package pl.vistula.helloworldtatarkiewicz;
+
+public class HelloWorldTatarkiewicz {
+    public static void main(String[] args) {
+        System.out.println("HelloVistulaTatarkiewcz");
+       
+    }
+}
